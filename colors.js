a18b506c29cab5860efe33a36f533a5f5cb17507
Arush Shah <arushshah1@gmail.com>
2017-03-17 21:19:20 -0400

    init

---
+++ colors.js
@@ -0,0 +1,18 @@
+var colors = ["red", "blue", "green", "yellow", "pink", "orange", "gold", "gray", "lightblue"];
+
+function changeColor() {
+    var rand = Math.floor(Math.random() * (9 - 0 + 1));
+    document.getElementById("bo").style.backgroundColor = colors[rand];
+}
+
+function changeTextColor() {
+    var rand = Math.floor(Math.random() * (9 - 0 + 1));
+    document.getElementById("dank").style.color = colors[rand];
+}
+
+function meat() {
+    document.getElementById("bo").style.backgroundImage = "url(ayy.gif)";
+}
+
+setInterval(changeColor, 100);
+setInterval(changeTextColor, 100);
